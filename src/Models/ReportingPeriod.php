8be82f1fc4a6ebd1fdd0eee71281bb1508d4ed8c
Raymund Delfin <raymund.delfin@appetiser.com.au>
2026-01-01 09:40:53 +0800

    fix: Add closing_date to ReportingPeriod fillable fields

--- src/Models/ReportingPeriod.php
+++ src/Models/ReportingPeriod.php
@@ -69,6 +69,7 @@ class ReportingPeriod extends Model implements Segregatable, Recyclable
         'calendar_year',
         'status',
         'entity_id',
+        'closing_date',
     ];
 
     /**
